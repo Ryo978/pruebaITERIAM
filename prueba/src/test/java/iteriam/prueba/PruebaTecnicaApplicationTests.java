51018c17eb35040b3c31e5cc75677a055b3bdb14
José Ayllón <jose.ayllonb@um.es>
2023-04-20 23:24:15 +0200

    added tests

--- prueba/src/test/java/iteriam/prueba/PruebaTecnicaApplicationTests.java
+++ prueba/src/test/java/iteriam/prueba/PruebaTecnicaApplicationTests.java
@@ -1,6 +1,7 @@
 package iteriam.prueba;
 
 import iteriam.prueba.error.BadOperatorException;
+import iteriam.prueba.error.OperationException;
 import iteriam.prueba.services.CalculatorService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,9 +23,9 @@ class PruebaTecnicaApplicationTests {
 
 	@Test
 	void lessSuccessTest(){
-		double addTest = 26.5;
+		double lessTest = 26.5;
 		double result = service.resolveSimpleOperation(50.9, 24.4, "-");
-		Assertions.assertEquals(addTest, result);
+		Assertions.assertEquals(lessTest, result);
 	}
 
 	@Test
@@ -34,4 +35,24 @@ class PruebaTecnicaApplicationTests {
 		});
 	}
 
+	@Test
+	void complexOperationTest() {
+		double operationTest = 22.9;
+		double result = service.resolveComplexOperation(" 14.0+20.9- 12");
+		Assertions.assertEquals(operationTest, result);
+	}
+
+	@Test
+	void badRegexOperationTest(){
+		Assertions.assertThrows(OperationException.class, () -> {
+			service.resolveComplexOperation("a + 675.00-5*b");
+		});
+	}
+
+	@Test
+	void badDoubleOperationTest(){
+		Assertions.assertThrows(OperationException.class, () -> {
+			service.resolveComplexOperation("12+4.56+3..8");
+		});
+	}
 }
